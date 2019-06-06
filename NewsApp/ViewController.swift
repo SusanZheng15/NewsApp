905a5a1042ec3e6b6b06adfaeea90eb558b2f86c
Susan Zheng <Zheng.susan12@gmail.com>
2019-06-06 19:27:39 -0400

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Susan Zheng on 6/6/19.
+//  Copyright © 2019 Susan Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
